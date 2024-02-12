ffaf1959e107acf08a23dbbeeb6ad52b20696128
stappost <emanuele.scarpantoni@gmail.com>
2024-02-12 15:27:28 +0100

    add img and header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,80 @@
+<div class="bg-primary">
+    <div class="container">
+        <div class="row ">
+            <div class="col-12">
+                <ul class="list-unstyled text-white d-flex my-0 justify-content-end gap-3">
+                    <li class="fw-semibold fs-6 me-5">DC POWER VISA</li>
+                    <li class="fw-semibold fs-6">ADDICTIONAL DC SITES</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li class="uppercase">
+                            <a href="">
+                                characters
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                comics
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                movies
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                tv
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                games
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                collectibles
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                videos
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                fans
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                news
+                            </a>
+                        </li>
+                        <li class="uppercase">
+                            <a href="">
+                                shop
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search">
+                    <input type="text">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
